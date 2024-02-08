35df0f5a1bdde37e3e3936433e0842a1ca02371f
Orisadek <ori.sadek@gmail.com>
2024-02-08 16:30:00 +0200

    fixed errors apb

--- apb_slave_module.v
+++ apb_slave_module.v
@@ -11,7 +11,7 @@
 `resetall
 `timescale 1ns/10ps
 
-module apb_slave_module(clk_i,rst_ni,psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,pready_o,pslverr_o,prdata_o,busy_o);
+module apb_slave_module(clk_i,rst_ni,psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,pready_o,pslverr_o,prdata_o,busy_o); // ports
 input clk_i,rst_ni,psel_i,penable_i,pwrite_i; // input ports
 input pstrb_i,pwdata_i,paddr_i; // input ports
 output pready_o,pslverr_o,prdata_o,busy_o; // output ports
@@ -19,9 +19,9 @@ parameter DATA_WIDTH = 32; // data width
 parameter BUS_WIDTH = 64; // bus width
 parameter ADDR_WIDTH = 32; // address width
 localparam MAX_DIM = BUS_WIDTH/DATA_WIDTH; // max dim of the matrix/ max dim of the matrix  NEVER USED
-parameter [1:0] IDLE          = 2'b00,
-			    ACCESS_READ   = 2'b01,
-				ACCESS_WRITE  = 2'b10;					      
+parameter [1:0] IDLE          = 2'b00, // idle state
+			    ACCESS_READ   = 2'b01, // read state
+				ACCESS_WRITE  = 2'b10; // write state				      
 	
 
 wire clk_i, rst_ni; // define clk and rst
@@ -42,11 +42,11 @@ always @(psel_i,penable_i,current_state)	// combinatorical always
       		IDLE: // idle
       			begin
       				if(psel_i) //if pslverr_o==1 then a reset is needed, can be changed if we want that pslverr_o	will not stop the APB			          				  
-        				begin
-      				       pready_next = 0; 
-        				   next_busy   = 1;  
-						   pslverr_next = 0;
-          					if(pwrite_i)
+        				begin 
+      				       pready_next  = 0;  // not ready
+        				   next_busy    = 1;  // is busy
+						   pslverr_next = 0; // no error
+          					if(pwrite_i) 	// if pwrite == 1
             					begin
 									next_state = ACCESS_WRITE; // next state is write
 								end
@@ -68,47 +68,34 @@ always @(psel_i,penable_i,current_state)	// combinatorical always
 									next_state   = penable_i ? IDLE : ACCESS_READ; // go to idle / stay
 									next_busy    = penable_i ? 0: 1;  //  busy
 									pslverr_next = 1'b0; // without an err
-									/*
-									if(penable_i) // enable read 
-										begin
-											pready_next  = 1;  // allow next op
-											prdata_next  = RAM[paddr_i]; //insert the data from ram to prdata_o bus.
-											next_state   = IDLE; // go to idle
-											next_busy    = 0;  // isnt busy
-											pslverr_next = 1'b0; // without an err
-										end
-										*/
-								end
+								end // end if
 							else
 								begin //if the psel fall betwine clocks the transaction is an error
           						next_state   = IDLE; // go to idle
           						pslverr_next = 1'b1; //either the sel fall during clocks or strob is not 0, transaction is not valid.
           						prdata_next  = 0; // data 0	
-    					   end
-					end
-      			end
+    					   end // end else
+					end // end if
+      			end // end read
       			
       		ACCESS_WRITE: // write state
 			    begin
 					if(next_state==ACCESS_WRITE) // if we are in write
 						begin
-							if(psel_i)
+							if(psel_i) // if pasel == 1
 								begin
-									if(penable_i) //enable write
-										begin
-											pready_next      = 1; // allow next op
-											next_state       = IDLE; // go to idle
-											next_busy        = 0;  // isnt busy
-											pslverr_next     = 1'b0;  // without an err
-										end
-								end
+									pready_next  = penable_i ? 1 : 0;  // allow next op
+									next_state   = penable_i ? IDLE : ACCESS_WRITE; // go to idle / stay
+									next_busy    = penable_i ? 0: 1;  //  busy
+									pslverr_next = 1'b0; // without an err
+								end // end if
 							else
 								begin //if the psel fall during the transaction its an error
           						next_state   = IDLE; // go to idle
           						pslverr_next = 1'b1; // err
-							end
-						end
-				end  
+							end // end else
+						end // end if
+				end  // end write
           
           default:
                 next_state = IDLE;  // move to idle
@@ -116,16 +103,17 @@ always @(psel_i,penable_i,current_state)	// combinatorical always
     end	
 
 
-genvar b;
-generate for(b=0;b<MAX_DIM;b=b+1) 
+genvar b; // b variable
+generate  // grenerate the block
+  for(b = 0 ; b < MAX_DIM ; b = b+1) // for loop
     begin:insert_byte
-        always @(psel_i,penable_i,current_state)
-          begin:strobe_apb
-            if(current_state == ACCESS_WRITE && pstrb_i[b] && penable_i)
-              begin
-                RAM[paddr_i][(b+1)*DATA_WIDTH-1:b*DATA_WIDTH] <= pwdata_i[(b+1)*DATA_WIDTH-1:b*DATA_WIDTH];
-              end
-          end
+        always @(psel_i,penable_i,current_state)// sensitivity list
+			begin:strobe_apb
+				if(current_state == ACCESS_WRITE && pstrb_i[b] && penable_i) //if we writing and in strobe and enabled
+					begin
+						RAM[paddr_i][(b+1)*DATA_WIDTH-1:b*DATA_WIDTH] <= pwdata_i[(b+1)*DATA_WIDTH-1:b*DATA_WIDTH]; // write data
+					end
+			end
     end 
 endgenerate
 
@@ -137,21 +125,20 @@ begin: apb_clk
 		if(!rst_ni) //reset, go back to idle and reset all outputs.
     		begin
     			current_state    <= IDLE; // state idle
-    			pready_o 	       <= 1;   // ready 1
-    			pslverr_o	       <= 0;   // err 0
-    			prdata_o 	       <= 0;   // out data 0
+    			pready_o 	     <= 1;   // ready 1
+    			pslverr_o	     <= 0;   // err 0
+    			prdata_o 	     <= 0;   // out data 0
     			busy_o           <= 0;	// busy 0
     		end
 	    else 
 			begin
-				current_state    <= next_state; // move to next state
-    			pready_o 	        <= pready_next; // move to next ready
-    			pslverr_o	        <= pslverr_next; // move to next pslverr
-    			prdata_o 	        <= prdata_next; // move to next out data
-    			busy_o            <= next_busy; // move to next busy
+				current_state   <= next_state; // move to next state
+    			pready_o 	    <= pready_next; // move to next ready
+    			pslverr_o	    <= pslverr_next; // move to next pslverr
+    			prdata_o 	    <= prdata_next; // move to next out data
+    			busy_o          <= next_busy; // move to next busy
 			end
 end
 endmodule
-// ### Please start your Verilog code here ### 
 
 
